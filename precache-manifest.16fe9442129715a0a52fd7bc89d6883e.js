7e19b99190cb822f5b6c6fb8652bf947935d43f0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-22 07:42:26 +0000

    Deploy rayspock/your-own-mnemonic to github.com/rayspock/your-own-mnemonic.git:gh-pages

---
+++ precache-manifest.16fe9442129715a0a52fd7bc89d6883e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0757b6828ef22e142e9339c0d7376d4",
+    "url": "/your-own-mnemonic/index.html"
+  },
+  {
+    "revision": "c846dc0813802c61dd74",
+    "url": "/your-own-mnemonic/static/css/2.29c27c91.chunk.css"
+  },
+  {
+    "revision": "c846dc0813802c61dd74",
+    "url": "/your-own-mnemonic/static/js/2.ff79a696.chunk.js"
+  },
+  {
+    "revision": "bcae926b778c723a9bee761295b60d5b",
+    "url": "/your-own-mnemonic/static/js/2.ff79a696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5efda95b84ee1794364b",
+    "url": "/your-own-mnemonic/static/js/main.d6b96f7e.chunk.js"
+  },
+  {
+    "revision": "5d7bf7a0464c2b9726e0",
+    "url": "/your-own-mnemonic/static/js/runtime-main.7f7c5e18.js"
+  }
+]);
